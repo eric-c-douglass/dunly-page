2cd3e247f015ebc69b55bc40c8369479e229a57d
Eric Douglass <eric.c.douglass@gmail.com>
2025-06-06 11:38:24 -0500

    Another logo fix

--- next.config.ts
+++ next.config.ts
@@ -1,11 +1,22 @@
 /** @type {import('next').NextConfig} */
+const isGithubActions = process.env.GITHUB_ACTIONS || false;
+
+let assetPrefix = '';
+let basePath = '';
+
+if (isGithubActions && process.env.GITHUB_REPOSITORY) {
+  const repo = process.env.GITHUB_REPOSITORY.replace(/.*?\//, '');
+  assetPrefix = `/${repo}/`;
+  basePath = `/${repo}`;
+}
+
 const nextConfig = {
   output: 'export',
   images: {
     unoptimized: true,
   },
-  basePath: process.env.NODE_ENV === 'production' ? '/dunly-page' : '',
-  assetPrefix: process.env.NODE_ENV === 'production' ? '/dunly-page/' : '',
+  assetPrefix: assetPrefix,
+  basePath: basePath,
   trailingSlash: true,
 };
 
